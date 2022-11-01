6826105a4fe9b58342726d8a375700a8a7a45ba9
Darya Ruban <ruban1ds@gmail.com>
2022-11-01 12:37:17 +0300

    hw_24

---
+++ hw_24/cypress.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'cypress';
+import {
+  assetsFolder,
+  baseUrl,
+  defaultWaitingTime,
+} from './cypress/support/constants/constants';
+
+export default defineConfig({
+  e2e: {
+    specPattern: 'hw_24/cypress/e2e/**/*.cy.ts',
+    baseUrl,
+    defaultCommandTimeout: defaultWaitingTime * 5,
+    supportFile: 'hw_24/cypress/support/index.ts',
+    videosFolder: `${assetsFolder}/videos`,
+    downloadsFolder: `${assetsFolder}/downloads`,
+    screenshotsFolder: `${assetsFolder}/screenshots`,
+    fixturesFolder: 'hw_24/cypress/fixtures',
+  },
+});
